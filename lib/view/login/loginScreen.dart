d41ce790af2c6d1548ac308d0540ae6cd62b97b4
arinming <jyandysys1@naver.com>
2023-10-20 17:02:36 +0900

    [feat] : HTTP 통신을 GetX 요청으로 변경 (#4)

--- lib/view/login/loginScreen.dart
+++ lib/view/login/loginScreen.dart
@@ -1,10 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:http/http.dart' as http;
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
+import 'package:mojacknong_android/view/login/CustomInterceptor.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Dio dio = Dio(
+  BaseOptions(
+    baseUrl: "http://ec2-43-202-6-54.ap-northeast-2.compute.amazonaws.com",
+  ),
+);
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -41,15 +48,17 @@ class LoginScreen extends StatelessWidget {
 
   void getKakaoLogin() async {
     print("카카오 로그인 버튼 클릭");
+    dio.interceptors.add(CustomInterceptor());
+
     bool isInstalled = await isKakaoTalkInstalled();
     OAuthToken? token;
 
     if (isInstalled) {
       try {
         token = await UserApi.instance.loginWithKakaoTalk();
-        print('카카오톡으로 로그인 성공');
+        print('카카오톡으로 로그인 성공1');
       } catch (error) {
-        print('카카오톡으로 로그인 실패 $error');
+        print('카카오톡으로 로그인 실패1 $error');
 
         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
@@ -59,25 +68,26 @@ class LoginScreen extends StatelessWidget {
         // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
         try {
           token = await UserApi.instance.loginWithKakaoAccount();
-          print('카카오계정으로 로그인 성공');
+          print('카카오계정으로 로그인 성공2');
         } catch (error) {
-          print('카카오계정으로 로그인 실패 $error');
+          print('카카오계정으로 로그인 실패2 $error');
         }
       }
     } else {
       try {
         token = await UserApi.instance.loginWithKakaoAccount();
-        print('카카오계정으로 로그인 성공');
+        print('카카오계정으로 로그인 성공3');
         print(token.accessToken);
-        fetchSecureData(token.accessToken);
+        fetchData(token.accessToken);
       } catch (error) {
-        print('카카오계정으로 로그인 실패 $error');
+        print('카카오계정으로 로그인 실패3 $error');
       }
     }
   }
 
   Future<void> getGoogleLogin() async {
     print("구글 로그인 버튼 클릭");
+    dio.interceptors.add(CustomInterceptor());
 
     final GoogleSignInAccount? googleSignInAccount =
         await googleSignIn.signIn();
@@ -85,24 +95,23 @@ class LoginScreen extends StatelessWidget {
     final GoogleSignInAuthentication googleSignInAuthentication =
         await googleSignInAccount!.authentication;
 
-    print(googleSignInAuthentication.accessToken);
-    fetchSecureData(googleSignInAuthentication.accessToken.toString());
+    print("구글 액세스 토큰 ${googleSignInAuthentication.accessToken}");
+    fetchData(googleSignInAuthentication.accessToken);
+    print(googleSignInAccount);
+    print(googleSignInAuthentication);
   }
 
-  Future fetchSecureData(token) async {
-    final url = Uri.parse(
-        'http://ec2-43-202-6-54.ap-northeast-2.compute.amazonaws.com/api/user/auth/kakao-login');
-
-    print(token);
-    final response = await http.post(
-      url,
-      headers: {'Authentication': "Bearer $token"},
-    );
-
-    if (response.statusCode == 200) {
-      print(response.body);
-    } else {
-      print('Error: ${response.statusCode}');
+  Future<void> fetchData(token) async {
+    try {
+      Response response = await dio.post(
+        '/api/user/auth/kakao-login',
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      print(response.data);
+      print("성공");
+    } on DioError catch (e) {
+      print(e.message);
+      print("실패");
     }
   }
 }
