65529ab7743df989718c351ed0581d4dd641d3c4
arinming <jyandysys1@naver.com>
2023-10-29 10:42:22 +0900

    [feat] : 로그인  API 호출 부분 data, repository 이동 (#10)

--- lib/repository/login_repository.dart
+++ lib/repository/login_repository.dart
@@ -1,9 +1,23 @@
 import 'package:mojacknong_android/data/network/api_service.dart';
-import 'package:mojacknong_android/res/app_url/app_url.dart';
 
 class LoginRepository {
-  static Future<dynamic> hitApi() async {
-    var response = await ApiServices().getApi(AppUrl.url);
+  static Future<dynamic> kakaoLoginApi(token) async {
+    var response = await ApiServices().fetchKaKaoData(token);
+    return response;
+  }
+
+  static Future<dynamic> googleLoginApi() async {
+    var response = await ApiServices().getGoogleLogin();
+    return response;
+  }
+
+  static Future<dynamic> reissueApi() async {
+    var response = await ApiServices().reissue();
+    return response;
+  }
+
+  static Future<dynamic> logoutApi() async {
+    var response = await ApiServices().logout();
     return response;
   }
 }
--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -1,4 +1,4 @@
 class AppUrl {
   static const url =
-      "http://ec2-13-125-15-222.ap-northeast-2.compute.amazonaws.com/api/user/logout";
+      "http://ec2-13-125-15-222.ap-northeast-2.compute.amazonaws.com";
 }
