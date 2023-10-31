033c615bf4c7406f953d12a844160ad9eb7abcd5
arinming <jyandysys1@naver.com>
2023-10-31 18:24:22 +0900

    [feat] : 폰트, 컬러, 로고 추가 (#14)

---
+++ lib/common/farmus_theme_data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FarmusThemeData {
+  static const Color grey1 = Color(0xFFF9F9F9);
+  static const Color grey2 = Color(0xFFF0F0F0);
+  static const Color grey3 = Color(0xFFC0C0C0);
+  static const Color grey4 = Color(0xFF7D7D7D);
+  static const Color grey5 = Color(0xFF383838);
+  static const Color grey6 = Color(0xFF1D1D1D);
+
+  static const Color green1 = Color(0xFF184B2D);
+  static const Color green2 = Color(0xFF50BF73);
+  static const Color green3 = Color(0xFFDFF9BA);
+
+  static const Color beige1 = Color(0xFFBCAC8D);
+  static const Color beige2 = Color(0xFFCCBEA4);
+  static const Color beige3 = Color(0xFFEAE4E1);
+}
