c802d87ed571250d66c6665b92597fe812c46138
arinming <jyandysys1@naver.com>
2023-11-03 17:09:20 +0900

    [feat] : 온보딩 레이아웃 구현 (#16)

--- lib/view/onboarding/onboarding_screen.dart
+++ lib/view/onboarding/onboarding_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:mojacknong_android/view/onboarding/component/next_button.dart';
-import 'package:mojacknong_android/view/onboarding/component/onboarding_finish.dart'; // Importing OnboardingFinish
+import 'package:mojacknong_android/view/onboarding/component/onboarding_finish.dart';
 import 'package:mojacknong_android/view/onboarding/component/onboarding_first.dart';
-import 'package:mojacknong_android/view/onboarding/component/onboarding_fourth.dart'; // Importing OnboardingFourth
-import 'package:mojacknong_android/view/onboarding/component/onboarding_second.dart'; // Importing OnboardingSecond
-import 'package:mojacknong_android/view/onboarding/component/onboarding_third.dart'; // Importing OnboardingThird
+import 'package:mojacknong_android/view/onboarding/component/onboarding_fourth.dart';
+import 'package:mojacknong_android/view/onboarding/component/onboarding_second.dart';
+import 'package:mojacknong_android/view/onboarding/component/onboarding_third.dart';
 import 'package:mojacknong_android/view/onboarding/component/previous_button.dart';
 import 'package:mojacknong_android/view/onboarding/component/progress_bar.dart';
 
@@ -20,6 +20,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   String title = "프로필과 나의 닉네임을 정해주세요.";
   String description = "";
   late Widget _currentWidget;
+  bool isLastPage = false;
 
   @override
   void initState() {
@@ -33,6 +34,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         if (_currentPage < 5) {
           _currentPage += 1;
         } else {
+          isLastPage = true;
           return;
         }
       } else {
@@ -43,26 +45,28 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         }
       }
 
+      isLastPage = false;
+
       if (_currentPage == 1) {
-        _currentWidget = OnboardingFirst();
         title = "프로필과 나의 닉네임을 정해주세요.";
         description = "";
+        _currentWidget = OnboardingFirst();
       } else if (_currentPage == 2) {
-        _currentWidget = OnboardingSecond();
         title = "홈파밍을 하는 이유가 궁금해요!";
         description = "내 홈파밍 동기에 맞는 응원 메시지를 전해드릴게요";
+        _currentWidget = OnboardingSecond();
       } else if (_currentPage == 3) {
-        _currentWidget = OnboardingThird();
         title = "작물과 함께 할 수 있는 시간을 알려주세요!";
         description = "아직 작물을 고르지 못했다면 팜어스가 추천해드릴게요";
+        _currentWidget = OnboardingThird();
       } else if (_currentPage == 4) {
-        _currentWidget = OnboardingFourth();
         title = "작물을 얼마나 잘 기르시나요?";
         description = "아직 작물을 고르지 못했다면 팜어스가 추천해드릴게요";
+        _currentWidget = OnboardingFourth();
       } else if (_currentPage == 5) {
-        _currentWidget = OnboardingFinish();
         title = "파머님 가입이 완료되었어요!";
         description = "홈에서 추천 작물을 확인해보세요";
+        _currentWidget = OnboardingFinish();
       }
     });
   }
@@ -127,7 +131,9 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                       onPressed: () => _handleButtonPress(false),
                     ),
                   ),
-                  const SizedBox(width: 16),
+                  SizedBox(
+                    width: 16,
+                  ),
                   Expanded(
                     child: NextButton(
                       onPressed: () => _handleButtonPress(true),
