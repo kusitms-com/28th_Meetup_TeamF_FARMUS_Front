064ab5908f9af496fa61416b493b0b94200e7fb7
arinming <jyandysys1@naver.com>
2023-11-03 16:14:02 +0900

    [feat] : 온보딩 화면 이동에 따른 텍스트 변경 (#16)

--- lib/view/onboarding/onboarding_screen.dart
+++ lib/view/onboarding/onboarding_screen.dart
@@ -17,6 +17,8 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   int _currentPage = 1;
+  String title = "프로필과 나의 닉네임을 정해주세요.";
+  String description = "";
   late Widget _currentWidget;
 
   @override
@@ -25,36 +27,42 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     _currentWidget = OnboardingFirst();
   }
 
-  void _changeContent() {
+  void _handleButtonPress(bool isNext) {
     setState(() {
-      if (_currentPage < 5) {
-        _currentPage += 1;
-        if (_currentPage == 2) {
-          _currentWidget = OnboardingSecond();
-        } else if (_currentPage == 3) {
-          _currentWidget = OnboardingThird();
-        } else if (_currentPage == 4) {
-          _currentWidget = OnboardingFourth();
+      if (isNext) {
+        if (_currentPage < 5) {
+          _currentPage += 1;
         } else {
-          _currentWidget = OnboardingFinish();
+          return;
+        }
+      } else {
+        if (_currentPage > 1) {
+          _currentPage -= 1;
+        } else {
+          return;
         }
       }
-    });
-  }
 
-  void _goBack() {
-    setState(() {
-      if (_currentPage > 1) {
-        _currentPage -= 1;
-        if (_currentPage == 1) {
-          _currentWidget = OnboardingFirst();
-        } else if (_currentPage == 2) {
-          _currentWidget = OnboardingSecond();
-        } else if (_currentPage == 3) {
-          _currentWidget = OnboardingThird();
-        } else {
-          _currentWidget = OnboardingFourth();
-        }
+      if (_currentPage == 1) {
+        _currentWidget = OnboardingFirst();
+        title = "프로필과 나의 닉네임을 정해주세요.";
+        description = "";
+      } else if (_currentPage == 2) {
+        _currentWidget = OnboardingSecond();
+        title = "홈파밍을 하는 이유가 궁금해요!";
+        description = "내 홈파밍 동기에 맞는 응원 메시지를 전해드릴게요";
+      } else if (_currentPage == 3) {
+        _currentWidget = OnboardingThird();
+        title = "작물과 함께 할 수 있는 시간을 알려주세요!";
+        description = "아직 작물을 고르지 못했다면 팜어스가 추천해드릴게요";
+      } else if (_currentPage == 4) {
+        _currentWidget = OnboardingFourth();
+        title = "작물을 얼마나 잘 기르시나요?";
+        description = "아직 작물을 고르지 못했다면 팜어스가 추천해드릴게요";
+      } else if (_currentPage == 5) {
+        _currentWidget = OnboardingFinish();
+        title = "파머님 가입이 완료되었어요!";
+        description = "홈에서 추천 작물을 확인해보세요";
       }
     });
   }
@@ -72,10 +80,39 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             ),
             Padding(
               padding: const EdgeInsets.only(top: 16.0),
-              child: ProgressBar(currentPage: _currentPage),
+              child: ProgressBar(
+                currentPage: _currentPage,
+              ),
             ),
             const SizedBox(
               height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+              ),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Pretendard",
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 8.0,
+              ),
+              child: Text(
+                description,
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontFamily: "Pretendard",
+                ),
+                textAlign: TextAlign.start,
+              ),
             ),
             Expanded(
               child: _currentWidget,
@@ -85,19 +122,17 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  if (_currentPage != 1)
-                    Expanded(
-                      child: PreviousButton(
-                        onPressed: _goBack,
-                      ),
+                  Expanded(
+                    child: PreviousButton(
+                      onPressed: () => _handleButtonPress(false),
                     ),
-                  if (_currentPage != 5) const SizedBox(width: 16),
-                  if (_currentPage != 5)
-                    Expanded(
-                      child: NextButton(
-                        onPressed: _changeContent,
-                      ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: NextButton(
+                      onPressed: () => _handleButtonPress(true),
                     ),
+                  ),
                 ],
               ),
             ),
