b3003a629713945437829651244052b8b24603a0
geonheey <ghyjas0704@gmail.com>
2023-11-07 00:25:36 +0900

    [feat] : 더보기 클릭 시 각 히스토리 페이지로 이동

--- lib/view/my_page/history/my_farm_history.dart
+++ lib/view/my_page/history/my_farm_history.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mojacknong_android/common/custom_app_bar.dart';
 
-class MyPageList extends StatelessWidget {
-  const MyPageList({super.key});
+class MyFarmHistory extends StatelessWidget {
+  const MyFarmHistory({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
       body: const Center(
-        child: Text("--"),
+        child: Text("팜 히스토리"),
       ),
     );
   }
