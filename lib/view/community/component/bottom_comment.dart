9f22b7daec68a4486f0324ec35cb2a23d47a22b1
arinming <jyandysys1@naver.com>
2023-11-06 22:01:17 +0900

    [feat] : GetX 컨트롤러로 상태관리 구현 (#20)

--- lib/view/community/component/bottom_comment.dart
+++ lib/view/community/component/bottom_comment.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 import 'package:mojacknong_android/common/farmus_theme_data.dart';
+import 'package:mojacknong_android/view_model/controllers/community_detail_controller.dart';
 
 class BottomComment extends StatefulWidget {
   @override
@@ -10,47 +12,50 @@ class BottomComment extends StatefulWidget {
 final int maxLength = 100;
 
 class _BottomCommentState extends State<BottomComment> {
-  String textValue = "";
+  final CommunityDetailController _controller =
+      Get.put(CommunityDetailController());
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 16.0, bottom: 16.0, right: 16.0),
-          child: TextFormField(
-            decoration: InputDecoration(
-              hintText: '댓글을 입력하세요',
-              hintStyle: TextStyle(
-                color: FarmusThemeData.dark.withOpacity(0.3),
-              ),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30.0),
-                borderSide: BorderSide.none,
-              ),
-              counterText: "",
-              filled: true,
-              fillColor: FarmusThemeData.background,
-              suffix: Text("${textValue.length} / $maxLength"),
-              suffixStyle: TextStyle(
-                color: FarmusThemeData.dark.withOpacity(0.3),
-              ),
-              suffixIcon: IconButton(
-                onPressed: () {},
-                icon: SvgPicture.asset(
-                  "assets/image/ic_arrow_up.svg",
+    return GetBuilder<CommunityDetailController>(
+      builder: (_) {
+        return Stack(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 16.0, bottom: 16.0, right: 16.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: '댓글을 입력하세요',
+                  hintStyle: TextStyle(
+                    color: FarmusThemeData.dark.withOpacity(0.3),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  counterText: "",
+                  filled: true,
+                  fillColor: FarmusThemeData.background,
+                  suffix: Obx(() => Text(
+                      "${_controller.textValue.value.length} / $maxLength")),
+                  suffixStyle: TextStyle(
+                    color: FarmusThemeData.dark.withOpacity(0.3),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      "assets/image/ic_arrow_up.svg",
+                    ),
+                  ),
                 ),
+                maxLength: maxLength,
+                onChanged: _controller.updateTextValue,
               ),
             ),
-            maxLength: maxLength,
-            onChanged: (value) {
-              setState(() {
-                textValue = value;
-              });
-            },
-          ),
-        ),
-      ],
+          ],
+        );
+      },
     );
   }
 }
---
+++ lib/view_model/controllers/community_detail_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class CommunityDetailController extends GetxController {
+  final textValue = "".obs;
+
+  void updateTextValue(String value) {
+    textValue.value = value;
+  }
+}
