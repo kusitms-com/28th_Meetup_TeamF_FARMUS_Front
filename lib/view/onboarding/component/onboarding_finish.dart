146288126edef3301d2e99ce9a0ff2923bfcd537
arinming <jyandysys1@naver.com>
2023-11-08 02:39:40 +0900

    [feat] : 온보딩 종료 일러스트 변경 (#25)

--- lib/view/onboarding/component/onboarding_finish.dart
+++ lib/view/onboarding/component/onboarding_finish.dart
@@ -23,33 +23,43 @@ class _OnboardingFinishState extends State<OnboardingFinish> {
           Container(
               width: double.infinity,
               height: MediaQuery.of(context).size.height / 2, // 전체 화면 크기의 1/2
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: FarmusThemeData.green1,
-              ),
+
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  SvgPicture.asset("assets/image/logo_onboarding.svg"),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  const Text(
-                    "성취감과 즐거움을 느낄 수 있도록\n파머님의 홈파밍 여정에 함께할게요",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: FarmusThemeData.primary,
-                        fontSize: 13,
-                        fontFamily: "Pretendard"),
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  SvgPicture.asset(
-                    "assets/image/logo_farmus.svg",
-                    width: 67,
-                    height: 14.82,
+                  Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/image/logo_onboarding.svg",
+                        width: 256,
+                        height: 256,
+                      ),
+                      Column(
+                        children: [
+                          SvgPicture.asset(
+                            "assets/image/logo_farmus.svg",
+                            width: 85.91,
+                            height: 19,
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                          const Text(
+                            "성취감과 즐거움을 느낄 수 있도록\n파머님의 홈파밍 여정에 함께할게요",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: FarmusThemeData.primary,
+                                fontSize: 13,
+                                fontFamily: "Pretendard"),
+                          ),
+                          const SizedBox(
+                            height: 24,
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               )),
