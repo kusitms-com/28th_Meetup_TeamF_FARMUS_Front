e513506ea4b0c143d43aaab0badd35b8894daa7c
geonheey <ghyjas0704@gmail.com>
2023-11-17 04:01:16 +0900

    [feat] : 달력 구현 중(미완성) (#58)

---
+++ lib/view/home/component/mission_routine/multi_calendar.dart
@@ -0,0 +1,174 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:mojacknong_android/common/farmus_theme_data.dart';
+import 'package:mojacknong_android/view/home/component/mission_routine/utils.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class TableMultiExample extends StatefulWidget {
+  const TableMultiExample({super.key});
+
+  @override
+  _TableMultiExampleState createState() => _TableMultiExampleState();
+}
+
+class _TableMultiExampleState extends State<TableMultiExample> {
+  final ValueNotifier<List<Event>> _selectedEvents = ValueNotifier([]);
+
+  // Using a `LinkedHashSet` is recommended due to equality comparison override
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Set<DateTime> days) {
+    // Implementation example
+    // Note that days are in selection order (same applies to events)
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      _focusedDay = focusedDay;
+      // Update values in a Set
+      if (_selectedDays.contains(selectedDay)) {
+        _selectedDays.remove(selectedDay);
+      } else {
+        _selectedDays.add(selectedDay);
+      }
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("달력");
+    return Scaffold(
+      body: Column(
+        children: [
+          TableCalendar<Event>(
+            firstDay: DateTime.utc(2023, 1, 1),
+            lastDay: DateTime.utc(2025, 12, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            eventLoader: _getEventsForDay,
+            selectedDayPredicate: (day) {
+              // Use values from Set to mark multiple days as selected
+              return _selectedDays.contains(day);
+            },
+            onDaySelected: _onDaySelected,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Clear selection'),
+            onPressed: () {
+              setState(() {
+                _selectedDays.clear();
+                _selectedEvents.value = [];
+              });
+            },
+          ),
+          const SizedBox(height: 8.0),
+          Expanded(
+            child: ValueListenableBuilder<List<Event>>(
+              valueListenable: _selectedEvents,
+              builder: (context, value, _) {
+                return ListView.builder(
+                  itemCount: value.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 12.0,
+                        vertical: 4.0,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: ListTile(
+                        onTap: () => print('${value[index]}'),
+                        title: Text('${value[index]}'),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyRoutineCalendar extends StatelessWidget {
+  final DateTime focusedDay;
+  final DateTime? selectedDay;
+  final Function(DateTime, DateTime) onDaySelected;
+
+  const MyRoutineCalendar({
+    Key? key,
+    required this.focusedDay,
+    required this.selectedDay,
+    required this.onDaySelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2023, 1, 1),
+      lastDay: DateTime.utc(2025, 12, 31),
+      focusedDay: focusedDay,
+      calendarFormat: CalendarFormat.month,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      daysOfWeekVisible: true,
+      selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+      locale: 'ko_KR',
+      onDaySelected: onDaySelected,
+      calendarStyle: const CalendarStyle(
+        isTodayHighlighted: false,
+        selectedDecoration: BoxDecoration(
+          color: FarmusThemeData.brownButton,
+          shape: BoxShape.circle,
+        ),
+        selectedTextStyle: TextStyle(color: Colors.white),
+        defaultDecoration: BoxDecoration(
+          shape: BoxShape.circle,
+        ),
+      ),
+      headerStyle: const HeaderStyle(
+        formatButtonVisible: false,
+        titleCentered: true,
+        formatButtonShowsNext: false,
+        leftChevronVisible: false,
+        rightChevronVisible: false,
+      ),
+    );
+  }
+}
