69dfc4bfa746d1dfa4b64d6cc5593f10896e9198
arinming <jyandysys1@naver.com>
2023-11-22 02:56:48 +0900

    [feat] : 가입하기 API 요청 성공 (#90)

--- lib/view_model/controllers/farmclub/farmclub_join_controller.dart
+++ lib/view_model/controllers/farmclub/farmclub_join_controller.dart
@@ -6,6 +6,9 @@ class FarmclubJoinController extends GetxController {
   RxBool isCheck = RxBool(false);
   RxBool isFormValid = RxBool(false);
   RxList<VeggieRegistration> veggieRegistration = <VeggieRegistration>[].obs;
+  var veggieList = <VeggieRegistration>[].obs;
+  RxInt selectedVeggieIndex = RxInt(0);
+
 
   @override
   void onInit() {
@@ -16,6 +19,18 @@ class FarmclubJoinController extends GetxController {
     });
   }
 
+  void updateSelectedVeggie(int index) async {
+    if (index >= 0 && index < veggieList.length) {
+      // 새로운 팜클럽 선택
+      selectedVeggieIndex.refresh();
+      selectedVeggieIndex.value = index;
+
+      print("채소 ${selectedVeggieIndex.value}");
+
+      update();
+    }
+  }
+
   Future<List<VeggieRegistration>> getVeggieRegistration() async {
     try {
       List<VeggieRegistration> responseData =
@@ -24,6 +39,20 @@ class FarmclubJoinController extends GetxController {
       // RxList 갱신
       veggieRegistration.clear();
       veggieRegistration.addAll(responseData);
+      veggieList.value = responseData;
+
+      return responseData;
+    } catch (error) {
+      // 오류 처리 로직 추가
+      print('Error fetching farmclub data: $error');
+      throw error;
+    }
+  }
+
+  Future<String> postFarmclubRegister() async {
+    try {
+      String responseData =
+      await FarmclubRepository.postRegister("1", veggieList[selectedVeggieIndex.toInt()].veggieId.toString());
 
       return responseData;
     } catch (error) {
