7ccf451936930499c712a96f5951005db5c2198a
geonheey <ghyjas0704@gmail.com>
2023-11-22 03:58:12 +0900

    [feat] : 스플래시, 온보딩, 로그인 완료 (#92)

--- lib/view/newonboarding/onboard_screen.dart
+++ lib/view/newonboarding/onboard_screen.dart
@@ -1,14 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk_talk.dart';
 import 'package:mojacknong_android/common/bouncing.dart';
 import 'package:mojacknong_android/model/farmus_user.dart';
+import 'package:mojacknong_android/repository/login_repository.dart';
+import 'package:mojacknong_android/view/login/app_interceptor.dart';
+import 'package:mojacknong_android/view/main/main_screen.dart';
 import 'package:mojacknong_android/view/newonboarding/onboard_first.dart';
 import 'package:mojacknong_android/view/newonboarding/onboard_fourth.dart';
 import 'package:mojacknong_android/view/newonboarding/onboard_second.dart';
 import 'package:mojacknong_android/view/newonboarding/onboard_third.dart';
+import 'package:mojacknong_android/view/onboarding/onboarding_screen.dart';
 
 final GoogleSignIn googleSignIn = GoogleSignIn();
 const storage = FlutterSecureStorage();
@@ -25,14 +31,14 @@ Dio authDio = Dio(
   ),
 );
 
-class OnBoardingScreen extends StatefulWidget {
-  const OnBoardingScreen({Key? key}) : super(key: key);
+class OnBoardScreen extends StatefulWidget {
+  const OnBoardScreen({Key? key}) : super(key: key);
 
   @override
-  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+  _OnBoardScreenState createState() => _OnBoardScreenState();
 }
 
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
+class _OnBoardScreenState extends State<OnBoardScreen> {
   late FarmusUser user;
   final PageController _pageController = PageController();
   final List<String> _pageContents = ['Page 1', 'Page 2', 'Page 3', 'Page 4'];
@@ -40,7 +46,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // authDio.interceptors.add(AppInterceptor(authDio));
+    authDio.interceptors.add(AppInterceptor(authDio));
 
     return Scaffold(
         body: Stack(
@@ -81,7 +87,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             onPress: () {},
             child: GestureDetector(
               onTap: () {
-                // kakaoLogin();
+                kakaoLogin();
               },
               child: SvgPicture.asset(
                 "assets/image/kakao_login.svg",
@@ -98,7 +104,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             onPress: () {},
             child: GestureDetector(
               onTap: () {
-                // googleLogin();
+                googleLogin();
               },
               child: SvgPicture.asset(
                 "assets/image/google_login.svg",
@@ -133,104 +139,105 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       ),
     );
   }
+
+  kakaoLogin() async {
+    print("카카오 로그인 버튼 클릭");
+
+    bool isInstalled = await isKakaoTalkInstalled();
+    OAuthToken? token;
+
+    if (isInstalled) {
+      try {
+        token = await UserApi.instance.loginWithKakaoTalk();
+        print('카카오톡으로 로그인 성공1');
+        fetchKaKaoData(token.accessToken);
+      } catch (error) {
+        print('카카오톡으로 로그인 실패1 $error');
+
+        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return;
+        }
+        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
+        try {
+          token = await UserApi.instance.loginWithKakaoAccount();
+          print('카카오계정으로 로그인 성공2');
+          fetchKaKaoData(token.accessToken);
+        } catch (error) {
+          print('카카오계정으로 로그인 실패2 $error');
+        }
+      }
+    } else {
+      try {
+        token = await UserApi.instance.loginWithKakaoAccount();
+        print('카카오계정으로 로그인 성공3');
+        print(token.accessToken);
+        fetchKaKaoData(token.accessToken);
+      } catch (error) {
+        print('카카오계정으로 로그인 실패3 $error');
+      }
+    }
+  }
+
+  fetchKaKaoData(token) {
+    LoginRepository.kakaoLoginApi(token).then(
+      (value) {
+        setState(() {
+          user = value;
+        });
+        print(value.early);
+        print(value.nickName);
+
+        if (value.early == true) {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const OnBoardScreen()),
+          );
+        } else {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const MainScreen()),
+          );
+        }
+      },
+    );
+  }
+
+  googleLogin() async {
+    print("구글 로그인 버튼 클릭");
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await googleSignIn.signIn();
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount!.authentication;
+
+    print("구글 액세스 토큰 ${googleSignInAuthentication.accessToken}");
+    LoginRepository.googleLoginApi(googleSignInAuthentication.accessToken).then(
+      (value) {
+        setState(() {
+          user = value;
+        });
+        print("초기 로그인 ${value.early}");
+        print(value.nickName);
+
+        if (value.early == true) {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+          );
+        } else {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => const MainScreen()),
+          );
+        }
+      },
+    );
+  }
+
+  reissue() {
+    LoginRepository.reissueApi().then((value) {});
+  }
+
+  logout() {
+    LoginRepository.logoutApi().then((value) {});
+  }
 }
-
-// kakaoLogin() async {
-//   print("카카오 로그인 버튼 클릭");
-
-//   bool isInstalled = await isKakaoTalkInstalled();
-//   OAuthToken? token;
-
-//   if (isInstalled) {
-//     try {
-//       token = await UserApi.instance.loginWithKakaoTalk();
-//       print('카카오톡으로 로그인 성공1');
-//       fetchKaKaoData(token.accessToken);
-//     } catch (error) {
-//       print('카카오톡으로 로그인 실패1 $error');
-
-//       // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-//       // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-//       if (error is PlatformException && error.code == 'CANCELED') {
-//         return;
-//       }
-//       // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-//       try {
-//         token = await UserApi.instance.loginWithKakaoAccount();
-//         print('카카오계정으로 로그인 성공2');
-//         fetchKaKaoData(token.accessToken);
-//       } catch (error) {
-//         print('카카오계정으로 로그인 실패2 $error');
-//       }
-//     }
-//   } else {
-//     try {
-//       token = await UserApi.instance.loginWithKakaoAccount();
-//       print('카카오계정으로 로그인 성공3');
-//       print(token.accessToken);
-//       fetchKaKaoData(token.accessToken);
-//     } catch (error) {
-//       print('카카오계정으로 로그인 실패3 $error');
-//     }
-//   }
-// }
-
-// fetchKaKaoData(token) {
-//   LoginRepository.kakaoLoginApi(token).then(
-//     (value) {
-//       setState(() {
-//         user = value;
-//       });
-//       print(value.early);
-//       print(value.nickName);
-
-//       if (value.early == true) {
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => const OnboardingScreen()),
-//         );
-//       } else {
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => const MainScreen()),
-//         );
-//       }
-//     },
-//   );
-// }
-
-// googleLogin() async {
-//   print("구글 로그인 버튼 클릭");
-
-//   final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
-
-//   final GoogleSignInAuthentication googleSignInAuthentication =
-//       await googleSignInAccount!.authentication;
-
-//   print("구글 액세스 토큰 ${googleSignInAuthentication.accessToken}");
-//   LoginRepository.googleLoginApi(googleSignInAuthentication.accessToken).then(
-//     (value) {
-//       setState(() {
-//         user = value;
-//       });
-//       print("초기 로그인 ${value.early}");
-//       print(value.nickName);
-
-//       if (value.early == true) {
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => const OnboardingScreen()),
-//         );
-//       } else {
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => const MainScreen()),
-//         );
-//       }
-//     },
-//   );
-// }
-
-// reissue() {
-//   LoginRepository.reissueApi().then((value) {});
-// }
-
-// logout() {
-//   LoginRepository.logoutApi().then((value) {});
-// }
--- lib/view/newonboarding/splash_screen.dart
+++ lib/view/newonboarding/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
       const Duration(seconds: 4),
       () => Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const OnBoardingScreen()),
+        MaterialPageRoute(builder: (context) => const OnBoardScreen()),
       ),
     );
   }
