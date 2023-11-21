5ac0cb845c89ad749dfe65221664e1cc9daa126f
arinming <jyandysys1@naver.com>
2023-11-21 14:23:07 +0900

    [feat] : 팜클런 일기 스크린 API 매핑 및 추천 팜클럽 유저 닉네임 매핑 (#85)

--- lib/view_model/controllers/farmclub/farmclub_controller.dart
+++ lib/view_model/controllers/farmclub/farmclub_controller.dart
@@ -22,6 +22,7 @@ class FarmclubController extends GetxController {
       selectedFarmclubIndex.value = index;
       selectedFarmclubIndex.refresh();
       print("gggg ${selectedFarmclubIndex.value}");
+      getMyFarmclub();
     }
   }
 
