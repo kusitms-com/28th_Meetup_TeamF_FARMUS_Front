fd6f2b719df4ecc2eeee80d5e05d58d3eceb2a98
arinming <jyandysys1@naver.com>
2023-11-23 17:02:49 +0900

    [feat] : 팜클럽 성공시 데이터 통신 성공 (#107)

--- lib/view_model/controllers/farmclub/farmclub_auth_controller.dart
+++ lib/view_model/controllers/farmclub/farmclub_auth_controller.dart
@@ -3,11 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mojacknong_android/model/farmclub_complete.dart';
-import 'package:mojacknong_android/common/bottom_sheet/farmclub_finish_final.dart';
 import 'package:mojacknong_android/view_model/controllers/bottom_sheet_controller.dart';
 
 import '../../../common/dialog/dialog_mission_farmclub.dart';
-import '../../../common/farmus_theme_data.dart';
 import '../../../model/farmclub_mission_response.dart';
 import '../../../repository/farmclub_repository.dart';
 
@@ -18,7 +16,6 @@ class FarmclubAuthController extends GetxController {
   final isFormValid = RxBool(false);
   Rx<FarmclubMissionData?> farmclubMission = Rx<FarmclubMissionData?>(null);
   RxBool missionUploaded = RxBool(false);
-  RxBool isEnd = RxBool(false);
   RxBool isLoading = RxBool(false); // 추가: 로딩 상태를 표시하는 변수
   BottomSheetController controller = BottomSheetController();
   Rx<FarmclubComplete?> farmclubComplete = Rx<FarmclubComplete?>(null);
@@ -46,21 +43,23 @@ class FarmclubAuthController extends GetxController {
     isFormValid.value = contentValue.isNotEmpty && image.value != null;
   }
 
-  Future<void> postFarmclubMission(BuildContext context, int registrationId,
-      String content, File image) async {
+
+  Future<void> postFarmclubMission(
+      int registrationId, String content, File image) async {
     try {
       isLoading.value = true;
 
       FarmclubMissionResponse response =
-          await FarmclubRepository.postFarmclubMission(
-              registrationId, content, image);
+      await FarmclubRepository.postFarmclubMission(
+          registrationId, content, image);
 
       farmclubMission.value = response.data;
       missionUploaded.value = true;
 
       if (farmclubMission.value!.isEnd == true) {
-        deleteFarmclubComplete(context);
+        deleteFarmclubComplete();
       }
+
     } catch (e) {
       print("오류: $e");
     } finally {
@@ -68,7 +67,7 @@ class FarmclubAuthController extends GetxController {
     }
   }
 
-  Future<void> deleteFarmclubComplete(BuildContext context) async {
+  Future<void> deleteFarmclubComplete() async {
     try {
       print(registrationId);
       FarmclubComplete? farmclubCompleteData =
@@ -77,40 +76,8 @@ class FarmclubAuthController extends GetxController {
       farmclubComplete.refresh();
       farmclubComplete.value = farmclubCompleteData;
       print(farmclubComplete.value!.challengeName.toString());
-
     } catch (e) {
       print("오류: $e");
     }
-  }
-
-  void showMissionFinishDialog(
-      BuildContext context,
-      String image,
-      String challengeName,
-      String day,
-      String mission,
-      String diary,
-      ) {
-    showModalBottomSheet<void>(
-      context: context,
-      isScrollControlled: true,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(40.0),
-          topRight: Radius.circular(40.0),
-        ),
-      ),
-      backgroundColor: FarmusThemeData.white,
-      builder: (BuildContext context) {
-        return SizedBox.expand(
-          child: BottomSheetFarmClubFinal(
-            imagePath: image,
-            title: challengeName,
-            textContent:
-            '텃밭부터 식탁까지 이어진 ${day}일의\n팜클럽 기간동안 ${mission}번 미션 인증을 했고\n${diary}번 성장일기를 기록했어요\n\n앞으로의 홈파밍 여정도 팜어스와 함께해요!',
-          ),
-        );
-      },
-    );
   }
 }
