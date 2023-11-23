ee0635b764c7fc812a1a4b6e6adc7b6e80320a1c
arinming <jyandysys1@naver.com>
2023-11-23 14:11:05 +0900

    [feat] : 팜클럽 가입시 해당 채소 아이디 데이터 필터링해서 띄우기 (#107)

--- lib/view/farmclub/component/explore/green_button.dart
+++ lib/view/farmclub/component/explore/green_button.dart
@@ -25,6 +25,7 @@ class _GreenButtonState extends State<GreenButton> {
       onPress: () {},
       child: GestureDetector(
         onTap: () async {
+          print(widget.veggieInfoId);
           await joinController.getVeggieRegistration(widget.veggieInfoId);
           controller.showFarmclubJoinBottomSheet(context, widget.challengeId);
         },
